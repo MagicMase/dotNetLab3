0c1dc1faf77955c8ec1ce900b0e50583d6272967
MagicMase <55172005+MagicMase@users.noreply.github.com>
2021-02-14 19:51:34 -0500

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using dotNetLab3.Models;
+
+
+namespace IntroductionToASP.NETMVCCore.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Razor()
+        {
+
+            return View();
+        }
+
+
+
+        public IActionResult Count()
+        {
+            ViewData["Bottles"] = Request.Form["Bottles"];
+            return View();
+        }
+
+
+
+        [HttpGet]
+        public IActionResult CreatePerson()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult DisplayPerson(Person model)
+        {
+
+            return View(model);
+        }
+
+
+
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotNetLab3.Models
+{
+    public class Person
+    {
+
+        [Required]
+        public string firstName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string lastName
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public int Age
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string Email
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime Birthday
+        {
+            get;
+            set;
+        }
+
+        [Required]
+        public string Password
+        {
+            get;
+            set;
+        }
+        [Required]
+        public string Description
+        {
+            get;
+            set;
+        }
+    }
+}
